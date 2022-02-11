1d95d91362abe06a4f4c148574672269883cfbd6
Dylan Trotter <dylan@bitcomplete.io>
2022-02-11 14:00:39 -0500

    plz v0.1.3

--- plz.rb
+++ plz.rb
@@ -3,9 +3,9 @@ require "formula"
 class Plz < Formula
     desc "plz CLI"
     homepage "https://plz.review"
-    version "0.1.2"
-    url "https://github.com/bitcomplete/plz-cli/releases/download/0.1.2/plz-darwin-amd64-0.1.2.tar.gz"
-    sha256 "1638333006df2b16993205a7bbac686ca7f6b4580d22d3f4fcfe7d2ee65fd654"
+    version "0.1.3"
+    url "https://github.com/bitcomplete/plz-cli/releases/download/0.1.3/plz-darwin-amd64-0.1.3.tar.gz"
+    sha256 "8e1d2fe0623cc27b3d2b76696b19595e588fdf82b4e064a4791ab7c4035bc004"
 
     bottle :unneeded
   
